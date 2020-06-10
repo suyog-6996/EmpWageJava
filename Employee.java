ffe77091a9ff4586e02764a9519c93e14bf02575
suyog-6996 <64637852+suyog-6996@users.noreply.github.com>
2020-06-10 10:06:29 +0530

    Updated

--- Employee.java
+++ Employee.java
@@ -2,7 +2,6 @@ package com.employee;
 import java.util.Random;
 public class Employee
 {
-    //Constants
     public static final int IS_EMP_FULL_TIME = 1;
     public static final int IS_EMP_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
@@ -10,15 +9,12 @@ public class Employee
     public static final int MAXIMUM_WORK_HOURS = 100;
     public static final int FULL_TIME_HOURS = 8;
     public static final int PART_TIME_HOURS = 4;
-
-    //variables
     public static int totalEmpHours = 0;
     public static int totalSalary = 0;
     public static int totalWorkingDays = 0;
     public static int empHours = 0;
     public static int DailyWage = 0;
-
-    //getting employee working hours
+    
     public static int getWorkingHours(int empCheck)
     {
         switch (empCheck)
@@ -35,14 +31,12 @@ public class Employee
         return empHours;
     }
 
-    //Calculating daily wage of employee
     public static int calculateDailyWage(int empHours)
     {
         DailyWage = empHours * EMP_RATE_PER_HOUR;
         return DailyWage;
     }
-
-    //calculating monthly wage of employee
+    
     public static void calculateMonthlyWage()
     {
         int empDailyWage[] = new int[100];
